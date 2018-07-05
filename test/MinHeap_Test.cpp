c0e047aa10cee55a259d25332f5c161ede40b62f
Trevor <whysoviet@gmail.com>
2018-07-05 16:59:02 -0700

    Reorganized tests, correctly added MinHeap_Test.cpp to executables

--- test/MinHeap_Test.cpp
+++ test/MinHeap_Test.cpp
@@ -8,6 +8,33 @@ TEST(MinHeap_Test, sizeIsZeroWhenDefaultConstructed)
 	EXPECT_EQ(0, mh.size());
 	EXPECT_TRUE(mh.isEmpty());
 }
+TEST(MinHeapTest, correctEmptySize)
+{
+	MinHeap<int> h = MinHeap<int>();
+	ASSERT_EQ(h.isEmpty(), true);
+	ASSERT_EQ(h.size(), 0);
+
+}
+
+TEST(MinHeapTest, addOne)
+{
+	MinHeap<int> h = MinHeap<int>();
+	h.add(1);
+	ASSERT_EQ(h.isEmpty(), false);
+	ASSERT_EQ(h.size(),1);
+	ASSERT_EQ(h.contains(1), true);
+	h.print();
+}
+
+TEST(MinHeapTest, addTwo)
+{
+	MinHeap<int> h = MinHeap<int>();
+	h.add(1);
+	h.add(0);
+	ASSERT_EQ(h.size(), 2);
+	ASSERT_EQ(h.contains(0), true);
+	h.print();
+}
 
 TEST(MinHeap_Test, nameATest)
 {
--- test/sample_test.cpp
+++ test/sample_test.cpp
@@ -10,30 +10,3 @@ TEST(sampleTestCaseName, sampleTestname)
 }
 
 
-TEST(MinHeapTest, correctEmptySize)
-{
-    MinHeap<int> h = MinHeap<int>();
-    ASSERT_EQ(h.isEmpty(), true);
-    ASSERT_EQ(h.size(), 0);
-
-}
-
-TEST(MinHeapTest, addOne)
-{
-    MinHeap<int> h = MinHeap<int>();
-    h.add(1);
-    ASSERT_EQ(h.isEmpty(), false);
-    ASSERT_EQ(h.size(),1);
-    ASSERT_EQ(h.contains(1), true);
-    h.print();
-}
-
-TEST(MinHeapTest, addTwo)
-{
-    MinHeap<int> h = MinHeap<int>();
-    h.add(1);
-    h.add(0);
-    ASSERT_EQ(h.size(), 2);
-    ASSERT_EQ(h.contains(0), true);
-    h.print();
-}
